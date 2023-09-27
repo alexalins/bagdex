2518aa0c06e9ae77ce3ac9c628c51bdf4380fb0a
alexalins <alexa@glinsoo.com>
2023-09-26 21:56:18 -0300

    criando jsonUtil

---
+++ src/main/java/com/alexalins/bagdex/domain/util/JsonUtil.java
@@ -0,0 +1,14 @@
+package com.alexalins.bagdex.domain.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public final class JsonUtil {
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
